9d7ecb3177a59d90a26d1917cf45d9bd446b7b11
Rafael Fracalossi <69407113+RemeLards@users.noreply.github.com>
2022-06-23 00:05:39 -0300

    Add files via upload

--- Kernighan/Chapter 1/KR_1_20.c
+++ Kernighan/Chapter 1/KR_1_20.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-/* tab ('\t') work as follow -> |-------|-------|-------|-------| ... */
-/* the tab goes to the next tabstop ( -> | ), so if there's a tab ('\t') after a tabstop, tab equals 8 blanks */
-/* if there's 1 char and a tab ('\t') , tab equals 7 blanks, if there's 2 chars and a tab, tab equals 6 blanks, and so on */
-#define TABSTOP 8 /* tabstop has a 8 char lenght */
+#define TABSTOP 8
 
 int distance_to_next_tab_stop(int num);
 
@@ -19,22 +16,12 @@ main()
         if(c == '\t')
         {
             TABSTOP_distance = distance_to_next_tab_stop(nc);
-
-            /* if its a tab after a tabstop, then this "if" will execute this exclusive case */
-            if(c == '\t' && TABSTOP_distance == 0)
-                for(i = 0; i < TABSTOP; i++)
-                {
-                    putchar(' ');
-                    ++nc;
-                }    
-
-            /* if "TABSTOP_distance" is 0, the above "if" execute the exclusive case */
-            else
-                for(i = 0; i < TABSTOP_distance; i++)
-                {
-                    putchar(' ');
-                    ++nc;
-                }    
+            
+            for(i = 0; i < TABSTOP_distance; i++)
+            {
+                putchar(' ');
+                ++nc;
+            }    
         }
         else if(c == '\n')
             {
@@ -50,9 +37,15 @@ main()
     return 0;
 }
 
-distance_to_next_tab_stop(int num)
+/* calculates the amount of chars needed to reach the next tabstop */ 
+int distance_to_next_tab_stop(int num)
 {
     while(num > 0)
         num = num - TABSTOP;
-    return -num;
+    
+    /* if "num = 0", this means the '\t' its after the Tabstop, so its value is equal to TABSTOP */
+    if(num == 0)
+        return TABSTOP;
+    else
+        return -num;
 }
